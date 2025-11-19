1d5c34c7162a274d6e7d9c0a9309ff0c52e71c04
Prathu3110 <170416389+Prathu3110@users.noreply.github.com>
2025-11-19 22:13:38 +0530

    disabling all control characters

--- femto.c
+++ femto.c
@@ -1,3 +1,4 @@
+#include<ctype.h>
 #include<stdio.h>
 #include<unistd.h>
 #include<termios.h>
@@ -15,8 +16,16 @@ void enableraw(){
     atexit(disableraw);
 
     struct termios raw=orig_termios;
-    raw.c_lflag &= ~(ECHO);
-    tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+    raw.c_iflag &= ~(ICRNL| IXON);
+    raw.c_lflag &= ~(ECHO |ICANON | ISIG |IEXTEN) ; 
+    
+    //ICANON- is used for canonical mode (which means input is read line by line) 
+    //ISIG is used for operations such INTEREPT SUSPEND TERMINATE (like ctrl-c, ctrl-z)
+    //IXON here is used for ctrl-s and ctrl-q which pauses and unpauses
+    //IEXTEN is for ctrl-v , and when you press ctrl-v before the next chartacter is taken as literal input , but now its is disabled
+    //ICRNL this here is used for disabling ctrl-m, stands for (CR- carriage return and NL= new line) this is used becasue ctrl-m inputs as 10 bytes while actually it should give 13 bytes
+
+    tcsetattr(STDIN_FILENO,TCSAFLUSH, &raw);
 
 }
 
@@ -24,7 +33,13 @@ void enableraw(){
 int main(){
     enableraw();
     char c;
-    while(read(STDIN_FILENO,&c,1)==1 && c!='q');
+    while(read(STDIN_FILENO,&c,1)==1 && c!='q'); //quits when q is entered
+    if(iscntrl(c)){
+        printf("%d \n", c);
 
+    }
+    else{
+        printf("%d ('%c')\n",c,c);
+    }
     return 0;
 }
